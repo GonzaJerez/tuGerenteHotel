8f7c7afa2095c4fe64d345d4300e58ae32b9d82e
Gonzalo <gonzalojerezn@gmail.com>
2023-01-25 17:45:17 -0300

    delete build directory

--- build/src/types/methodPayments.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.availableMethodPayments = void 0;
-exports.availableMethodPayments = ['CASH', 'CREDIT_CARD', 'DEBIT_CARD', 'TRANSFER'];
-//# sourceMappingURL=methodPayments.js.map
